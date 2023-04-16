14cb248be34e5c51a22c79e2140764711a5164f7
BhagyeshPro <bhagyeshpaunikar2001@gmail.com>
2023-04-15 18:32:11 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Test App Is here...
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          By Mihir
         </a>
       </header>
     </div>
